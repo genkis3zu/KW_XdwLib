41339f6381f4d12ecdd14de1bf0aa14abf229eec
genki <genkis3zu@gmail.com>
2021-03-04 17:12:38 +0900

    図面振り分けのシステムとしてDWFileDistributorを作成 振り分け方法の仕様を策定する必要があるのでどうするか 考えること

---
+++ KW_XdwLib/DWFileDistributor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KW_XdwLib
+{
+    /// <summary>
+    /// 指定のフォルダ内のDocuWorks文章を、指定の構成フォルダ内へと配布する。
+    /// 
+    /// </summary>
+    public class DWFileDistributor
+    {
+        private string _workspacePath;
+
+        public DWFileDistributor(string workspacePath)
+        {
+            _workspacePath = workspacePath;
+        }
+
+
+        public void DoDistribute()
+        {
+
+        }
+    }
+}
